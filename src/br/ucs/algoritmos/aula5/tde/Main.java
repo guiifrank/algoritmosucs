10451fb7401bf3136743255b26df2fa48c6d5060
guiifrank <gsusinfrank@gmail.com>
2024-04-09 19:37:51 -0300

    aula 5 : ajustes no TDE 2 referentes a leitura da palavra

--- src/br/ucs/algoritmos/aula5/tde/Main.java
+++ src/br/ucs/algoritmos/aula5/tde/Main.java
@@ -1,9 +1,13 @@
 package br.ucs.algoritmos.aula5.tde;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         PalindromeChecker.DoublyLinkedList list = new PalindromeChecker.DoublyLinkedList();
-        String text = "arara";
+        System.out.println("Digite a palavra que deseja verificar: ");
+        String text = sc.next();
         for (int i = 0; i < text.length(); i++) {
             list.append(text.charAt(i));
         }
